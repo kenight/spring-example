150d651048a97ea6c01308c6b2584fae2b55bb24
kenight <ikenight@gmail.com>
2017-09-06 23:28:44 +0800

    update

--- security-simple/src/main/java/com/example/simple/Application.java
+++ security-simple/src/main/java/com/example/simple/Application.java
@@ -3,14 +3,6 @@ package com.example.simple;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * 最简单的示例 <br>
- * Spring boot 项目加入 Security 后, 默认所以请求都需要认证通过后才能访问
- * 
- * 
- * @author MaiYo
- *
- */
 @SpringBootApplication
 public class Application {
 
