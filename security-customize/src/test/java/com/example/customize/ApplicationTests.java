9287029deb6aee419c363b870fdf8cc2e5f15789
kenight <ikenight@gmail.com>
2017-09-14 23:02:12 +0800

    update

--- security-customize/src/test/java/com/example/customize/ApplicationTests.java
+++ security-customize/src/test/java/com/example/customize/ApplicationTests.java
@@ -2,15 +2,25 @@ package com.example.customize;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ApplicationTests {
+	
+	@Autowired
+	private ApplicationContext ctx;
 
 	@Test
 	public void contextLoads() {
+		String[] beanNames = ctx.getBeanDefinitionNames();
+		System.out.println("bean count: " + beanNames.length);
+		for (String name : beanNames) {
+			System.out.println(name);
+		}
 	}
 
 }
